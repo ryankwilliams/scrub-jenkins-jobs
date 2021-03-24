e237eb2b03a681142ff4528e9f6e0921cec77efc
Ryan Williams <3375653+ryankwilliams@users.noreply.github.com>
2021-03-24 17:02:25 -0400

    Include base outline

---
+++ scrub_jenkins_jobs/scrub.py
@@ -0,0 +1,9 @@
+import click
+
+@click.command()
+def cli():
+    """Scrub Jenkins Jobs"""
+    pass
+
+if __name__ == "__main__":
+    cli()
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="scrub-jenkins-jobs",
+    version="0.0.0",
+    author="Ryan Williams",
+    description="A utility to remove stale Jenkins jobs",
+    url="https://github.com/ryankwilliams/scrub-jenkins-jobs",
+    install_requires=[
+        "click",
+        "python-jenkins"
+    ],
+    python_requires=">=3.6",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    entry_points={
+        "console_scripts": [
+            "scrub-jenkins-jobs=scrub_jenkins_jobs.scrub:cli"
+        ]
+    }
+)
